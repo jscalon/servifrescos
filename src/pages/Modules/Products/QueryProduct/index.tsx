199f4dbce09444cd99314f81ab402a7c93af8688
Juan <jgimenezeee@gmail.com>
2025-10-24 15:26:52 -0400

    Simulación de tabla de QueryProduct

--- src/pages/Modules/Products/QueryProduct/index.tsx
+++ src/pages/Modules/Products/QueryProduct/index.tsx
@@ -1,17 +1,40 @@
 import styles from "./QueryProduct.module.css";
 import QueryBar from "../../../../components/QueryBar";
-import Card from "../../../../components/Card";
-import Button from "../../../../components/Button";
-import InputField from "../../../../components/InputField";
 
 export default function QueryProduct() {
+  function arr(n: number) {
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+      arr.push(false);
+    }
+    return arr;
+  }
   return (
-    <>
-      <QueryBar></QueryBar>
+    <main className={styles.main}>
       <h1>Consultar Productos</h1>
-      <Card className={styles.card}>
-        <form action=""></form>
-      </Card>
-    </>
+      <QueryBar></QueryBar>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Marca</th>
+            <th>Tipo</th>
+            <th>Departamento</th>
+            <th>Grupo</th>
+            <th>Subgrupo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {arr(6).map(() => (
+            <tr>
+              {arr(7).map(() => (
+                <td></td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
 }
