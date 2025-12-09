fe87224000dbe3200cd43a08e665df8d68bc227e
Juan <jgimenezeee@gmail.com>
2025-12-09 11:30:09 -0400

    feat: Configurar para ejecutar docker desde cualquier pc

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { type AxiosResponse } from "axios";
 
-const API_BASE_URL = "http://172.20.12.33:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
