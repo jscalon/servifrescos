526a437f5df219127a83295dc20a89dee07676e0
Juan <jgimenezeee@gmail.com>
2025-12-22 14:00:32 -0400

    feat: Agregar gestión de productos desde la vista de admin de django

--- backend/apps/products/admin.py
+++ backend/apps/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['code', 'description', 'brand', 'type', 'department']
+    search_fields = ['code', 'description', 'brand']
+    list_filter = ['brand', 'type', 'department', 'group', 'subgroup']
+    ordering = ['code']
